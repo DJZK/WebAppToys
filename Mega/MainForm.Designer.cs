f7401f205d48e18e94fcbb6c47beefaccfcd69f5
DJZK <emperorxizzle99@gmail.com>
2022-02-15 17:03:12 +0800

    Finished mega browser

--- Mega/MainForm.Designer.cs
+++ Mega/MainForm.Designer.cs
@@ -29,23 +29,42 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MegaBrowser = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.MegaBrowser)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MegaBrowser
+            // 
+            this.MegaBrowser.CreationProperties = null;
+            this.MegaBrowser.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.MegaBrowser.Location = new System.Drawing.Point(0, -1);
+            this.MegaBrowser.Name = "MegaBrowser";
+            this.MegaBrowser.Size = new System.Drawing.Size(818, 503);
+            this.MegaBrowser.TabIndex = 0;
+            this.MegaBrowser.ZoomFactor = 1D;
+            this.MegaBrowser.CoreWebView2InitializationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs>(this.MegaBrowser_CoreWebView2InitializationCompleted);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(609, 396);
+            this.ClientSize = new System.Drawing.Size(818, 499);
+            this.Controls.Add(this.MegaBrowser);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(625, 435);
+            this.MinimumSize = new System.Drawing.Size(834, 538);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mega Browser";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.MegaBrowser)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 MegaBrowser;
     }
 }
 
--- Mega/MainForm.cs
+++ Mega/MainForm.cs
@@ -16,5 +16,38 @@ namespace Mega
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Text = "Mega Browser";
+            Icon = Properties.Resources.mega;
+            MegaBrowser.Size = Size;
+
+            MegaBrowser.Width = Width - 15;
+            MegaBrowser.Height = Height - 35;
+            MegaBrowser.Source = new Uri("https://mega.io");
+
+        }
+
+        private void MegaBrowser_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            MegaBrowser.CoreWebView2.Settings.AreDefaultScriptDialogsEnabled = false;
+            MegaBrowser.CoreWebView2.Settings.AreDevToolsEnabled = false;
+            MegaBrowser.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            MegaBrowser.CoreWebView2.Settings.IsStatusBarEnabled = false;
+
+            MegaBrowser.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+        }
+
+        private void CoreWebView2_NewWindowRequested(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            MegaBrowser.Width = Width - 15;
+            MegaBrowser.Height = Height - 35;
+        }
     }
 }
