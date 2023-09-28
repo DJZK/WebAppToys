25abb8bb24cde32aacaf61cdaca8679be8f724bc
DJZK <emperorxizzle99@gmail.com>
2023-09-28 13:23:42 +0800

    Modded currently working build

---
+++ Google Chat/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Mushroom
+{
+    public partial class MainForm : Form
+    {
+        private bool bypass = false, first = true;
+        private FormWindowState windowState;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Text = "Google Chat";
+            Icon = Google_Chat.Properties.Resources.Google_Chat;
+            MushroomBrowser.Size = Size;
+            notifyIcon1.Icon = Icon;
+            notifyIcon1.Visible = true;
+
+            MushroomBrowser.Width = Width - 15;
+            MushroomBrowser.Height = Height - 35;
+            MushroomBrowser.Source = new Uri("https://mail.google.com/chat");
+        }
+
+        private void CoreWebView2_NewWindowRequested(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!bypass)
+            {
+                e.Cancel = true;
+                windowState = this.WindowState;
+                WindowState = FormWindowState.Minimized;
+                notifyIcon1.Visible = true;
+
+                if (first)
+                {
+                    notifyIcon1.ShowBalloonTip(1000);
+                    first = false;
+                }
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            MushroomBrowser.Width = Width - 15;
+            MushroomBrowser.Height = Height - 35;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = windowState;
+        }
+
+        private void MushroomBrowser_CoreWebView2InitializationCompleted_1(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            MushroomBrowser.CoreWebView2.Settings.AreDefaultScriptDialogsEnabled = false;
+            MushroomBrowser.CoreWebView2.Settings.AreDevToolsEnabled = false;
+            MushroomBrowser.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            MushroomBrowser.CoreWebView2.Settings.IsStatusBarEnabled = false;
+
+            MushroomBrowser.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+        }
+
+        private void exiitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bypass = true;
+            Application.Exit();
+
+        }
+
+
+    }
+}
--- Google Chat/Program.cs
+++ Google Chat/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Google_Chat
+namespace Mushroom
 {
     internal static class Program
     {
@@ -14,9 +16,54 @@ namespace Google_Chat
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using (var mutex = new Mutex(false, "Mushroom"))
+            {
+                // TimeSpan.Zero to test the mutex's signal state and
+                // return immediately without blocking
+                bool isAnotherInstanceOpen = !mutex.WaitOne(TimeSpan.Zero);
+                if (isAnotherInstanceOpen)
+                {
+                    MessageBox.Show("Application already running!", "Application Running", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+
+                if (CheckInternet())
+                {
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+
+                    MessageBox.Show("There's no internet connection. Please connect to the internet and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+               
+
+                mutex.ReleaseMutex();
+            }
+
+        }
+
+        private static bool CheckInternet()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
